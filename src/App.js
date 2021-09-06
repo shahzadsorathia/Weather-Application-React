e0bcc573e2d16b3f18d543281f29e2633fff7823
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-06 01:46:22 -0400

    Update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Body from './Body';
+import Footer from "./Footer"
+import Header from './Header'
+function App() {
+  return (
+    <div className="App" >
+    <Header/>
+      <Body   />
+      <Footer />
+     
+</div>
+
+  );
+}
+
+export default App;
